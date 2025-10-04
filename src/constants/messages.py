a7459111f161d48964fdb766557a5f1d52a557a9
Josdasim <josdsim31@gmail.com>
2025-10-04 23:49:52 +0700

    feat: agrega validaciones basicas al modelo User con tests negativos (#2)

---
+++ src/constants/messages.py
@@ -0,0 +1,3 @@
+USER_INVALID_USERNAME = "Invalid username cannot be empty"
+USER_INVALID_EMAIL = "Invalid email address"
+USER_INVALID_PASSWORD = "Invalid password must be at least 6 characters"
--- tests/test_user_creation.py
+++ tests/test_user_creation.py
@@ -1,7 +1,24 @@
+import pytest
 from src.models.user import User
+
 def test_create_user_with_correct_attributes():
     user = User(username = "Tomas", email = "tomas01@correo.com", password = "secret01")
 
     assert user.username == "Tomas"
     assert user.email == "tomas01@correo.com"
     assert user.password == "secret01"
+
+def test_create_user_with_empty_username():
+    with pytest.raises(ValueError) as exc_info:
+        User(username = "", email = "tomas", password = "secret01")
+        assert "username" in str(exc_info.value)
+
+def test_create_user_with_invalid_email():
+    with pytest.raises(ValueError) as exc_info:
+        User(username = "Tomas", email = "email_invalido", password = "secret01")
+        assert "email" in str(exc_info.value)
+
+def test_create_user_with_invalid_password():
+    with pytest.raises(ValueError) as exc_info:
+        User(username = "Tomas", email = "tomas@correo.com", password = "sec01")
+        assert "password" in str(exc_info.value)
