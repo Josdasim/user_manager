47e8e3da0e25146fec05c67ad3145d0efa198e6a
Josdasim <josdsim31@gmail.com>
2025-10-07 01:09:29 +0700

    Feature/custom exceptions (#4)
    * Se crearon excepciones personalizadas, y se usaron modificando los archivo user.py y en su respectivo unit test
    
    * Modificacion de pipeline, cambio de @v2 -> @main
    
    * Se corrige escritura en el pipeline run-test -> run-tests
    
    * style: se realizan correcciones sugeridas por lint en (messages.py, user.py, user_exceptions.py, user_creatios)

---
+++ src/exceptions/user_exceptions.py
@@ -0,0 +1,9 @@
+class AppError(Exception):
+    def __init__(self, message: str):
+        super().__init__(message)
+        self.message = message
+
+
+class UserValidationError(AppError):
+    def __init__(self, message: str):
+        super().__init__(message) 
--- tests/test_user_creation.py
+++ tests/test_user_creation.py
@@ -1,24 +1,26 @@
 import pytest
 from src.models.user import User
+from src.exceptions.user_exceptions import UserValidationError
+
 
 def test_create_user_with_correct_attributes():
-    user = User(username = "Tomas", email = "tomas01@correo.com", password = "secret01")
+    user = User(username="Tomas", email="tomas01@correo.com", password="secret01")
 
     assert user.username == "Tomas"
     assert user.email == "tomas01@correo.com"
     assert user.password == "secret01"
 
 def test_create_user_with_empty_username():
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(UserValidationError) as exc_info:
         User(username = "", email = "tomas", password = "secret01")
         assert "username" in str(exc_info.value)
 
 def test_create_user_with_invalid_email():
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(UserValidationError) as exc_info:
         User(username = "Tomas", email = "email_invalido", password = "secret01")
         assert "email" in str(exc_info.value)
 
 def test_create_user_with_invalid_password():
-    with pytest.raises(ValueError) as exc_info:
+    with pytest.raises(UserValidationError) as exc_info:
         User(username = "Tomas", email = "tomas@correo.com", password = "sec01")
         assert "password" in str(exc_info.value)
