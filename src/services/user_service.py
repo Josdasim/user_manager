06aae35bbdd88ae2070858654dc1742c6af5beb4
JS <josdsim31@gmail.com>
2025-10-08 02:10:36 +0700

    feat: se crea clase UserRepository sus respectivas pruebas unitarias

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,17 +1,18 @@
 from src.models.user import User
 from src.exceptions.user_exceptions import UserValidationError
 from src.constants import messages
+from src.repositories.user_repository import UserRepository
 
 
 class UserService():
     
-    def __init__(self):
-        self._users = {}
+    def __init__(self, repository:UserRepository | None = None):
+        self.repository = repository or UserRepository()
 
     def create_user(self, username:str, email:str, password:str) -> User:
-        if username in self._users:
+        if self.repository.get(username):
             raise UserValidationError(messages.USER_ALREADY_EXISTS)
         
         user = User(username, email, password)
-        self._users[username] = user
+        self.repository.add(user)
         return user
