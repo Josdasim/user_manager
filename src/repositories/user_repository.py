dc9c988e709b727a803c01e36b0a7f3133dff140
JS <josdsim31@gmail.com>
2025-10-14 18:53:11 +0700

    refactor: se refactoriza la clase UserRepository

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -5,7 +5,7 @@ from src.exceptions.user_exceptions import UserValidationError, UserNotFoundErro
 
 class UserRepository():
     def __init__(self):
-        self._data = {}
+        self._data: dict[str, User] = {}
 
     #TODO: Se debe modificar para que retorne un User
     def add(self, user:User) -> None:
@@ -14,22 +14,21 @@ class UserRepository():
         self._data[user.username] = user
 
     def find(self, username:str) -> User | None:
-        return self._data.get(username)
+        return self._data.get(username.strip())
 
     def get(self, username:str) -> User:
         user = self.find(username)
-
         if not user:
             raise UserNotFoundError(messages.USER_NOT_FOUND)
         return user
     
     def update_email(self, username:str, new_email:str) -> User:
         user = self.get(username)
+        #TODO: Se debe buscar la forma de validar el correo antes de intentar actualiarlo
         user.email = new_email
         self._data[username] = user
         return user
 
-    #TODO: Mejorar el metodo quitando que lance el error al no obtener el usuario (Esto debe hacerse tras la mejora al metodo get)
     def delete(self, username:str)-> None:
         user = self.get(username)
         del self._data[username]
