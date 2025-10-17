6c50cd947584149e45a7a1da6825ece38518cd94
JS <josdsim31@gmail.com>
2025-10-18 03:18:02 +0700

    feat: Se agregan nuevos metodos y atributos al modelo 'User'

--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,8 @@
 import re
 from src.constants import messages
 from src.exceptions.user_exceptions import UserValidationError
-from src.security.password_utils import verify_password
+from uuid6 import uuid7
+from datetime import datetime
 
 
 class User:
@@ -11,19 +12,31 @@ class User:
     def __init__(self, username: str, email: str, password: str):
         self._validate_username(username)
         self._validate_email(email)
+        self.id = str(uuid7())
         self.username = username
         self.email = email
         self.password = password
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def _validate_username(self, username:str) -> None:
         if not username or not username.strip():
             raise UserValidationError(messages.USER_INVALID_USERNAME)
-        
-    #TODO: se podria considerar modificar(convencion del guion bajo inicial) para que pueda usarse en user_repository para la validacion de email    
+          
     def _validate_email(self, email:str) -> None:
         if not email or not re.match(self.EMAIL_PATTERN, email):
-            raise UserValidationError(messages.USER_INVALID_EMAIL)   
+            raise UserValidationError(messages.USER_INVALID_EMAIL)  
+
+    def _refresh_updated_at(self) -> None:
+        self.updated_at = datetime.now()
+    
+    #TODO: Change username, password
+    
+    def update_email(self, new_email:str)-> None:
+        self._validate_email(new_email)
+        self.email = new_email
+        self._refresh_updated_at()
         
     def __repr__(self) -> str:
-        return f"User(username='{self.username}', email='{self.email}')"
+        return f"User(username={self.username}, email={self.email}, created_at= {self.created_at}, updated_at={self.updated_at})"
         
