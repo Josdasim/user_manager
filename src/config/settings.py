03cfb6bc9f2af5bd41905c3b6a1acc6c3df4a6b4
JS <josdsim31@gmail.com>
2025-10-20 01:14:15 +0700

    fix: se corrige tipo en las variables de 'settings.py'

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Settings:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ALGORITHM = os.getenv("JWT_ALGORITHM")
-    JWT_EXPIRE_MINUTES = int(os.getenv("JWT_EXPIRE_MINUTES"))
+    JWT_EXPIRE_MINUTES = os.getenv("JWT_EXPIRE_MINUTES")
 
 settings = Settings()
