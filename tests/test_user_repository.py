c8a81c641e9a4cb961e825ab8f6530f6ead17674
Jose Simanca <josdsim31@gmail.com>
2025-10-20 19:58:40 -0500

    feat: Se crea clase 'RoleRepository' (#34)
    Se agregan los métodos (add, find, get, get_all, update_description y delete) en 'RoleRepositoy'. Además de sus respectivas pruebas.
    
    Archivos modificados:
    - role.py
    
    Archivos creados:
    - role_repository.py
    - test_role_repository.py

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -5,67 +5,67 @@ from src.constants import messages
 
 #---------------------ADD/GET---------------------
 
-def test_add_and_get_user(repo, sample_user_1):
-    repo.add(sample_user_1)
-    repo_retrieved = repo.get("Tomas")
+def test_add_and_get_user(user_repo, sample_user_1):
+    user_repo.add(sample_user_1)
+    repo_retrieved = user_repo.get("Tomas")
 
     assert repo_retrieved.username == sample_user_1.username
     assert repo_retrieved.email == sample_user_1.email
 
-def test_add_existing_user(repo, sample_user_2):
-    repo.add(sample_user_2)
+def test_add_existing_user(user_repo, sample_user_2):
+    user_repo.add(sample_user_2)
     with pytest.raises(UserValidationError, match = messages.USER_ALREADY_EXISTS):
-        repo.add(sample_user_2)
+        user_repo.add(sample_user_2)
     
-def test_get_nonexistent_user(repo):
+def test_get_nonexistent_user(user_repo):
     with pytest.raises(UserNotFoundError, match = messages.USER_NOT_FOUND):
-        repo.get("Unknown")
+        user_repo.get("Unknown")
 
 #---------------------FIND---------------------
 
-def test_find_user(repo, sample_user_3):
-    repo.add(sample_user_3)
-    assert repo.find("Axel") == sample_user_3
+def test_find_user(user_repo, sample_user_3):
+    user_repo.add(sample_user_3)
+    assert user_repo.find("Axel") == sample_user_3
 
-def test_user_not_found(repo):
-    assert repo.find("amiexist?") is None
+def test_user_not_found(user_repo):
+    assert user_repo.find("amiexist?") is None
 
 #--------------------UPDATE---------------------
 
-def test_update_existing_user(repo, sample_user_3):
-    repo.add(sample_user_3)
-    user_updated = repo.update_email(sample_user_3.username, "newaxel@correo.com")
-    assert repo.get(sample_user_3.username).email == "newaxel@correo.com"
+def test_update_existing_user(user_repo, sample_user_3):
+    user_repo.add(sample_user_3)
+    user_updated = user_repo.update_email(sample_user_3.username, "newaxel@correo.com")
+    assert user_repo.get(sample_user_3.username).email == "newaxel@correo.com"
 
-def test_update_nonexistent_user(repo):
+def test_update_nonexistent_user(user_repo):
     with pytest.raises(UserNotFoundError, match = messages.USER_NOT_FOUND):
-        repo.update_email("no_estoy", "correo@correo.com")
+        user_repo.update_email("no_estoy", "correo@correo.com")
     
 #--------------------DELETE-------------------
 
-def test_delete_exisiting_user(repo, sample_user_3):
-    repo.add(sample_user_3)
-    assert repo.get(sample_user_3.username) is not None 
+def test_delete_exisiting_user(user_repo, sample_user_3):
+    user_repo.add(sample_user_3)
+    assert user_repo.get(sample_user_3.username) is not None 
 
-    repo.delete("Axel")
-    assert repo.find("Axel") is None
+    user_repo.delete("Axel")
+    assert user_repo.find("Axel") is None
 
-def test_delete_nonexistent_user(repo):
+def test_delete_nonexistent_user(user_repo):
     with pytest.raises(UserNotFoundError, match = messages.USER_NOT_FOUND):
-        repo.delete("nonexistent")
+        user_repo.delete("nonexistent")
 
 #--------------------GET ALL-------------------
 
-def test_get_all_users(repo, sample_user_2, sample_user_3):
-    repo.add(sample_user_2)
-    repo.add(sample_user_3)
-    all_users = repo.get_all()
-    assert len(repo._data) == 2
+def test_get_all_users(user_repo, sample_user_2, sample_user_3):
+    user_repo.add(sample_user_2)
+    user_repo.add(sample_user_3)
+    all_users = user_repo.get_all()
+    assert len(user_repo._data) == 2
     assert len(all_users) == 2
     assert all_users[0].username == sample_user_2.username
     assert all_users[1].username == sample_user_3.username
 
-def test_get_all_without_users(repo):
-    all_users = repo.get_all()
+def test_get_all_without_users(user_repo):
+    all_users = user_repo.get_all()
     assert all_users == []
     assert len(all_users) == 0
--- tests/test_user_service.py
+++ tests/test_user_service.py
@@ -3,8 +3,8 @@ from src.exceptions.user_exceptions import UserValidationError, UserNotFoundErro
 from src.constants import messages
 
 
-def test_create_user_service(service, sample_user_data_2):
-    user = service.create_user(**sample_user_data_2)
+def test_create_user_service(user_service, sample_user_data_2):
+    user = user_service.create_user(**sample_user_data_2)
     assert user.username == sample_user_data_2["username"]
     assert user.email == sample_user_data_2["email"]
     assert user.password != sample_user_data_2["password"]
@@ -17,14 +17,14 @@ def test_create_user_service(service, sample_user_data_2):
         ], 
         ids = lambda val: ""
 )
-def test_create_user_with_invalid_field(service, username, email, password, expected_exception):
+def test_create_user_with_invalid_field(user_service, username, email, password, expected_exception):
     with pytest.raises(UserValidationError, match = expected_exception):
-        service.create_user(username, email, password)
+        user_service.create_user(username, email, password)
 
-def test_create_user_with_existing_username(service, sample_user_data_1):
-    service.create_user(**sample_user_data_1)
+def test_create_user_with_existing_username(user_service, sample_user_data_1):
+    user_service.create_user(**sample_user_data_1)
     with pytest.raises(UserValidationError, match = messages.USER_ALREADY_EXISTS):
-        service.create_user(**sample_user_data_1)
+        user_service.create_user(**sample_user_data_1)
 
 """def test_create_user_with_existing_email():
     service = UserService()
@@ -34,53 +34,53 @@ def test_create_user_with_existing_username(service, sample_user_data_1):
 
 #---------------------test_get_user------------------
 
-def test_get_existing_user(service, sample_user_data_1):
-    service.create_user(**sample_user_data_1)
-    user = service.get_user(sample_user_data_1["username"])
+def test_get_existing_user(user_service, sample_user_data_1):
+    user_service.create_user(**sample_user_data_1)
+    user = user_service.get_user(sample_user_data_1["username"])
     assert user.username == sample_user_data_1["username"]
     assert user.email == sample_user_data_1["email"]
 
-def test_get_unexistent_user(service):
+def test_get_unexistent_user(user_service):
     with pytest.raises(UserNotFoundError, match=messages.USER_NOT_FOUND):
-        service.get_user("axel")
+        user_service.get_user("axel")
 
 #--------------------test_update_user----------------
 
-def test_update_email_success(service, sample_user_data_2):
-    service.create_user(**sample_user_data_2)
-    result = service.update_email(sample_user_data_2["username"], "newemail@correo.com")
+def test_update_email_success(user_service, sample_user_data_2):
+    user_service.create_user(**sample_user_data_2)
+    result = user_service.update_email(sample_user_data_2["username"], "newemail@correo.com")
     assert result["username"] == sample_user_data_2["username"]
     assert result["email"] == "newemail@correo.com"
-    assert service.get_user("juan").email == "newemail@correo.com"
+    assert user_service.get_user("juan").email == "newemail@correo.com"
 
-def test_update_email_same_as_current(service, sample_user_data_1):
-    service.create_user(**sample_user_data_1)
+def test_update_email_same_as_current(user_service, sample_user_data_1):
+    user_service.create_user(**sample_user_data_1)
     with pytest.raises(SameEmailError, match=messages.SAME_EMAIL):
-        service.update_email(sample_user_data_1["username"], "xion@correo.com")
+        user_service.update_email(sample_user_data_1["username"], "xion@correo.com")
 
 #--------------------test_delete_user----------------
 
-def test_delete_user_success(service, sample_user_data_1):
-    service.create_user(**sample_user_data_1)
-    assert service.delete_user(sample_user_data_1["username"]) is None
+def test_delete_user_success(user_service, sample_user_data_1):
+    user_service.create_user(**sample_user_data_1)
+    assert user_service.delete_user(sample_user_data_1["username"]) is None
     with pytest.raises(UserNotFoundError, match=messages.USER_NOT_FOUND):
-        service.get_user(sample_user_data_1["username"])
+        user_service.get_user(sample_user_data_1["username"])
 
-def test_delete_nonexistent_user(service):
+def test_delete_nonexistent_user(user_service):
     with pytest.raises(UserNotFoundError, match=messages.USER_NOT_FOUND):
-        service.delete_user("iamnothere")
+        user_service.delete_user("iamnothere")
 
 #--------------------test_get_all_users----------------
 
-def test_get_all_users(service, sample_user_data_1,sample_user_data_2):
-    service.create_user(**sample_user_data_1)
-    service.create_user(**sample_user_data_2)
-    all_users = service.get_all_users()
+def test_get_all_users(user_service, sample_user_data_1,sample_user_data_2):
+    user_service.create_user(**sample_user_data_1)
+    user_service.create_user(**sample_user_data_2)
+    all_users = user_service.get_all_users()
     assert len(all_users) == 2
     assert all_users[0].username == sample_user_data_1["username"]
     assert all_users[1].username == sample_user_data_2["username"]
 
-def test_get_all_without_users(service):
-    all_users = service.get_all_users()
+def test_get_all_without_users(user_service):
+    all_users = user_service.get_all_users()
     assert all_users == []
     assert len(all_users) == 0
