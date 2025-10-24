f67816ec27ed5c1ba5e5a86fe3badbfda33a6b20
JS <josdsim31@gmail.com>
2025-10-24 07:52:08 +0700

    feat: Se modifico la clase 'User'

--- src/models/user.py
+++ src/models/user.py
@@ -18,7 +18,7 @@ class User:
         self.email = email
         self.password = password
         self.status = status
-        #TODO:Agregar nuevas variables para rol y permisos
+        self.roles: list[str] = []
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
@@ -33,15 +33,50 @@ class User:
     def _refresh_updated_at(self) -> None:
         self.updated_at = datetime.now()
     
-    #TODO: Change username, password
+    def update_username(self, new_username:str) -> None:
+        """Actualiza el nombre del usuario"""
+        self._validate_username(new_username)
+        self.username = new_username
+        self._refresh_updated_at()
     
     def update_email(self, new_email:str)-> None:
         self._validate_email(new_email)
         self.email = new_email
         self._refresh_updated_at()
     
-    #TODO: metodos para cambiar los estados del usuario
+    def update_password(self, new_password:str) -> None:
+        """Actualizar Contraseña del usuario (debe venir hasheada)"""
+        if not new_password:
+            raise UserValidationError(messages.USER_INVALID_PASSWORD)
+        self.password = new_password
+        self._refresh_updated_at()
+
+    #TODO: Métodos para gestionar los estados del usuario
+
+    def activate(self) -> None:
+        """Activa el usuario"""
+        self.status = UserStatus.ACTIVE
+        self._refresh_updated_at()
     
-    def __repr__(self) -> str:
-        return f"User(username={self.username}, email={self.email}, created_at= {self.created_at}, updated_at={self.updated_at})"
+    def deactivate(self) -> None:
+        """Desactiva el usuario"""
+        self.status = UserStatus.INACTIVE
+        self._refresh_updated_at()
+    
+    def suspend(self) -> None:
+        """Suspende el usuario"""
+        self.status = UserStatus.SUSPENDED
+        self._refresh_updated_at()
+    
+    def block(self) -> None:
+        """Bloquea el usuario"""
+        self.status = UserStatus.BLOCKED
+        self._refresh_updated_at()
+    
+    def is_active(self) -> bool:
+        """Verifica si el usuario está activo"""
+        return self.status == UserStatus.ACTIVE
+    
+    def _repr_(self) -> str:
+        return f"User(username={self.username}, email={self.email}, status={self.status}, created_at={self.created_at}, updated_at={self.updated_at})"
         
