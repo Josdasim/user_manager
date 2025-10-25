d8164f41219d2da09f5b97ead9817a4cdc8dbb80
JS <josdsim31@gmail.com>
2025-10-25 07:07:31 +0700

    refactor: Se mejora el manejo de excepciones en 'PermissionService'

--- src/services/permission_service.py
+++ src/services/permission_service.py
@@ -1,6 +1,7 @@
 from src.models.permission import Permission
 from src.repositories.permission_repository import PermissionRepository
-
+from src.exceptions.permission_exceptions import PermissionAlreadyExistsError, PermissionNotFoundError, PermissionValidationError
+from src.constants import messages
 
 class PermissionService:
     def __init__(self, repository: PermissionRepository | None = None):
@@ -8,15 +9,15 @@ class PermissionService:
 
     def create_permission(self, name: str, description: str = "") -> Permission:
         """Crea un nuevo permiso"""
-        if not name:
-            raise ValueError("El nombre del permiso es obligatorio")
+        if not name or not name.strip():
+            raise PermissionValidationError(messages.PERMISSION_INVALID_NAME)
         permission = Permission(name=name.strip().lower(), description=description)
         return self.repository.add(permission)
 
     def get_permission(self, name: str) -> Permission:
         """Obtiene un permiso por nombre"""
         if not name:
-            raise ValueError("El nombre del permiso es obligatorio")
+            raise PermissionValidationError(messages.PERMISSION_INVALID_NAME)
         return self.repository.get(name.strip().lower())
 
     def get_all_permissions(self) -> list[Permission]:
@@ -27,13 +28,13 @@ class PermissionService:
         """Actualiza la descripción de un permiso"""
         if not name:
             #TODO: crear error y mensaje personalizado (ej: Campos vacios | campos obligatorios)
-            raise ValueError("El nombre del permiso es obligatorio")
+            raise PermissionValidationError(messages.PERMISSION_INVALID_NAME)
         if new_description is None:
-            raise ValueError("La descripcion no puede ser nula")
+            raise PermissionValidationError(messages.PERMISSION_INVALID_TYPE)
         return self.repository.update_description(name.strip().lower(), new_description)
 
     def delete_permission(self, name: str) -> None:
         """Elimina un permiso"""
         if not name:
-            raise ValueError("El nombre del permiso es obligatorio")
+            raise PermissionValidationError(messages.PERMISSION_INVALID_NAME)
         self.repository.delete(name.strip().lower())
